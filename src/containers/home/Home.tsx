1fa73ad7b8adc3d258cc7e81665152aab69b8a33
dinhGiang1862 <giangdt@.onebox@gmail.com>
2024-04-14 15:10:51 +0700

    update

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,23 +1,22 @@
-import { COLORS } from '@/theme';
-import { observer } from 'mobx-react';
+import {COLORS, Styles} from '@/theme';
+import {observer} from 'mobx-react';
 import React from 'react';
-import {
-    StyleSheet, View, Text
-} from 'react-native';
-import StarIc from '@/assets/images/star.svg'
+import {StyleSheet, View, Text} from 'react-native';
+import StarIc from '@/assets/images/star.svg';
 
 const Home = observer(() => {
-    return (
-        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <Text >Home</Text>
-            <StarIc width={50} height={50}/>
-        </View>
-    );
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={styles.txtTitle}>Home</Text>
+      <StarIc width={50} height={50} />
+    </View>
+  );
 });
 
 export default Home;
 
-
 const styles = StyleSheet.create({
-   
+  txtTitle: {
+    ...Styles.typography.heading.h1_medium,
+  },
 });
--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,135 +1,159 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
-import { Configs } from '../commons/Configs';
-import { COLORS } from './colors';
+import {Configs} from '../commons/Configs';
+import {COLORS} from './colors';
 
 export const Styles = {
-    flexColumn: {
-        flex: 1
-    },
+  flexColumn: {
+    flex: 1,
+  },
 
-    typography: StyleSheet.create({
-        // white
-        regular: {
-            color: COLORS.BLACK,
-            fontSize: Configs.FontSize.size14,
-            fontFamily: Configs.FontFamily.regular
-        },
-        medium: {
-            color: COLORS.BLACK,
-            fontSize: Configs.FontSize.size14,
-            fontFamily: Configs.FontFamily.medium
-        },
-        bold: {
-            color: COLORS.BLACK,
-            fontSize: Configs.FontSize.size14,
-            fontFamily: Configs.FontFamily.bold
-        }
-    }),
-
-    /// //////
-    shadow: {
-        backgroundColor: COLORS.WHITE,
-        shadowColor: COLORS.BLACK,
-        shadowOffset: {
-            width: 0,
-            height: 0
-        },
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-        elevation: 1
+  typography: {
+    heading: {
+      h6_medium: {
+        color: COLORS.BLACK,
+        fontSize: Configs.FontSize.size14,
+        fontFamily: Configs.FontFamily.medium,
+      },
+      h5_medium: {
+        color: COLORS.BLACK,
+        fontSize: Configs.FontSize.size16,
+        fontFamily: Configs.FontFamily.medium,
+      },
+      h4_medium: {
+        color: COLORS.BLACK,
+        fontSize: Configs.FontSize.size18,
+        fontFamily: Configs.FontFamily.semiBold,
+      },
+      h3_medium: {
+        color: COLORS.BLACK,
+        fontSize: Configs.FontSize.size24,
+        fontFamily: Configs.FontFamily.bold,
+      },
+      h2_medium: {
+        color: COLORS.BLACK,
+        fontSize: Configs.FontSize.size32,
+        fontFamily: Configs.FontFamily.medium,
+      },
+      h1_medium: {
+        color: COLORS.BLACK,
+        fontSize: Configs.FontSize.size40,
+        fontFamily: Configs.FontFamily.semiBold,
+      },
     },
-    heavyShadow: {
-        backgroundColor: COLORS.WHITE,
-        shadowColor: COLORS.BLACK,
-        shadowOffset: {
-            width: 0,
-            height: 0
-        },
-        shadowOpacity: 1,
-        shadowRadius: 14,
-        elevation: 10
+    body: {
+      regular: {
+        color: COLORS.BLACK,
+        fontSize: Configs.FontSize.size14,
+        fontFamily: Configs.FontFamily.bold,
+      },
     },
-    textTransform: {
-        textTransform: 'uppercase'
-    }
+  },
 };
 
+/// //////
+//   shadow: {
+//     backgroundColor: COLORS.WHITE,
+//     shadowColor: COLORS.BLACK,
+//     shadowOffset: {
+//       width: 0,
+//       height: 0,
+//     },
+//     shadowOpacity: 0.3,
+//     shadowRadius: 4,
+//     elevation: 1,
+//   },
+//   heavyShadow: {
+//     backgroundColor: COLORS.WHITE,
+//     shadowColor: COLORS.BLACK,
+//     shadowOffset: {
+//       width: 0,
+//       height: 0,
+//     },
+//     shadowOpacity: 1,
+//     shadowRadius: 14,
+//     elevation: 10,
+//   },
+//   textTransform: {
+//     textTransform: 'uppercase',
+//   },
+// };
+
 export const HtmlStyles = {
-    a: {
-        ...Styles.typography.regular,
-        color: COLORS.BLACK,
-        fontSize: Configs.FontSize.size13,
-        textAlign: 'center'
-    },
-    b: {
-        ...Styles.typography.medium,
-        color: COLORS.BLACK,
-        fontSize: Configs.FontSize.size13,
-        textAlign: 'center'
-    },
-    w: {
-        ...Styles.typography.regular,
-        color: COLORS.WHITE,
-        fontSize: Configs.FontSize.size13,
-        textAlign: 'center'
-    },
-    g: {
-        ...Styles.typography.medium,
-        color: COLORS.GREEN,
-        fontSize: Configs.FontSize.size12
-    },
-    r: {
-        ...Styles.typography.medium,
-        color: COLORS.RED,
-        fontSize: Configs.FontSize.size13,
-        marginTop: 5
-    },
-    s: {
-        ...Styles.typography.regular,
-        fontSize: Configs.FontSize.size12,
-        padding: 15
-    },
-    t: {
-        ...Styles.typography.regular,
-        fontSize: Configs.FontSize.size12,
-        textAlign: 'center'
-    },
-    m: {
-        ...Styles.typography.medium,
-        fontSize: Configs.FontSize.size12,
-        color: COLORS.DARK_GRAY
-    },
-    p:{
-        ...Styles.typography.regular,
-        color: COLORS.RED,
-        fontSize: Configs.FontSize.size13
-    }
+  a: {
+    ...Styles.typography.regular,
+    color: COLORS.BLACK,
+    fontSize: Configs.FontSize.size13,
+    textAlign: 'center',
+  },
+  b: {
+    ...Styles.typography.medium,
+    color: COLORS.BLACK,
+    fontSize: Configs.FontSize.size13,
+    textAlign: 'center',
+  },
+  w: {
+    ...Styles.typography.regular,
+    color: COLORS.WHITE,
+    fontSize: Configs.FontSize.size13,
+    textAlign: 'center',
+  },
+  g: {
+    ...Styles.typography.medium,
+    color: COLORS.GREEN,
+    fontSize: Configs.FontSize.size12,
+  },
+  r: {
+    ...Styles.typography.medium,
+    color: COLORS.RED,
+    fontSize: Configs.FontSize.size13,
+    marginTop: 5,
+  },
+  s: {
+    ...Styles.typography.regular,
+    fontSize: Configs.FontSize.size12,
+    padding: 15,
+  },
+  t: {
+    ...Styles.typography.regular,
+    fontSize: Configs.FontSize.size12,
+    textAlign: 'center',
+  },
+  m: {
+    ...Styles.typography.medium,
+    fontSize: Configs.FontSize.size12,
+    color: COLORS.DARK_GRAY,
+  },
+  p: {
+    ...Styles.typography.regular,
+    color: COLORS.RED,
+    fontSize: Configs.FontSize.size13,
+  },
 };
 
 export const HtmlStylesSeen = {
-    w: {
-        ...Styles.typography.medium,
-        color: COLORS.GRAY_2,
-        fontSize: Configs.FontSize.size14,
-        marginTop: 5
-    },
-    b: {
-        ...Styles.typography.medium,
-        color: COLORS.GRAY_2,
-        fontSize: Configs.FontSize.size14,
-        marginTop: 5
-    },
-    span: {
-        ...Styles.typography.medium,
-        color: COLORS.BLUE,
-        fontSize: Configs.FontSize.size14,
-        marginTop: 5
-    },
-    a: {
-        ...Styles.typography.medium,
-        color: COLORS.BLUE,
-        fontSize: Configs.FontSize.size14,
-        marginTop: 5
-    }
+  w: {
+    ...Styles.typography.medium,
+    color: COLORS.GRAY_2,
+    fontSize: Configs.FontSize.size14,
+    marginTop: 5,
+  },
+  b: {
+    ...Styles.typography.medium,
+    color: COLORS.GRAY_2,
+    fontSize: Configs.FontSize.size14,
+    marginTop: 5,
+  },
+  span: {
+    ...Styles.typography.medium,
+    color: COLORS.BLUE,
+    fontSize: Configs.FontSize.size14,
+    marginTop: 5,
+  },
+  a: {
+    ...Styles.typography.medium,
+    color: COLORS.BLUE,
+    fontSize: Configs.FontSize.size14,
+    marginTop: 5,
+  },
 };
